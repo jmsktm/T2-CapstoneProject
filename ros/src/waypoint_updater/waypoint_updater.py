635878588dd0890dceccd5596d1b7a54c1386cce
iammsg <swethacitadel@gmail.com>
2019-05-04 10:51:04 -0400

    Updated waypoint_updater.py, dbw_node.py, twist_controller.py. Also added instructions to make this work in Udacity Workspace

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,60 +1,78 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, TwistStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from std_msgs.msg import Int32
 import math
+import numpy as np
+from scipy.spatial import KDTree
+
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
-
 As mentioned in the doc, you should ideally first implement a version which does not care
 about traffic lights or obstacles.
-
 Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-
 Please note that our simulator also provides the exact location of traffic lights and their
 current status in `/vehicle/traffic_lights` message. You can use this message to build this node
 as well as to verify your TL classifier.
-
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 200 # Number of waypoints we will publish.
+LOOKAHEAD_FILTER = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 16, 20, 28, 36, 52, 68, 100, 132, 196]
+FREQUENCY = 50 # 50Hz
 
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        # Adding other member variables needed
+        self.pose = None
+        #self.velocity = None
+        self.base_waypoints = None
+        self.waypoints_tree = None
+        self.stop_line_wp_idx = -1
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
-        rospy.spin()
+        # TODO: Add a subscriber for /obstacle_waypoint below
+
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(FREQUENCY)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints and self.waypoints_tree:
+                # Getting the final waypoints
+                final_waypoints = self.get_final_waypoints()
+                self.publish_waypoints(final_waypoints)
+            rate.sleep()
+
+    def publish_waypoints(self, final_waypoints):
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = final_waypoints
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.base_waypoints = waypoints
+        self.waypoints_tree = KDTree(
+            [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y]
+             for waypoint in waypoints.waypoints])
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
-
-    def obstacle_cb(self, msg):
-        # TODO: Callback for /obstacle_waypoint message. We will implement it later
-        pass
+        # Callback for /traffic_waypoint message.
+        self.stop_line_wp_idx = msg.data
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
@@ -62,14 +80,46 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
-    def distance(self, waypoints, wp1, wp2):
-        dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-            wp1 = i
-        return dist
+    def get_closest_waypoint_idx(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoints_tree.query([x, y], 1)[1]
 
+        # Checking if closest point is ahead or behind the vehicle
+        closest_waypoint = self.base_waypoints.waypoints[closest_idx]
+        prev_waypoint = self.base_waypoints.waypoints[
+            (closest_idx - 1) if closest_idx > 0 else (len(self.base_waypoints.waypoints) - 1)]
+            
+        closest_coord = [closest_waypoint.pose.pose.position.x, closest_waypoint.pose.pose.position.y]
+        prev_coord = [prev_waypoint.pose.pose.position.x, prev_waypoint.pose.pose.position.y]
+
+        # Equation for hyperplane through closest coords
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.base_waypoints.waypoints)
+        return closest_idx
+
+    def get_final_waypoints(self):
+        closest_idx = self.get_closest_waypoint_idx()
+        # We want the car to stop at the end of the track, so not doing module
+        farthest_idx = min(closest_idx + LOOKAHEAD_WPS, len(self.base_waypoints.waypoints))
+        return self.move_to_next_waypoint(closest_idx, farthest_idx)
+
+    def move_to_next_waypoint(self, closest_idx, farthest_idx):
+        final_waypoints = []
+
+        for i in LOOKAHEAD_FILTER:
+            idx = closest_idx + i
+            if idx < farthest_idx:
+                wp = self.base_waypoints.waypoints[idx]
+                final_waypoints.append(wp)
+
+        return final_waypoints
 
 if __name__ == '__main__':
     try:
